c933a3ced5b23a20cf7df539c42be8a5aa28b1a6
Danilo Higa <danikehiga@gmail.com>
2026-04-06 08:43:10 -0300

    exercicio 17

--- Java-repeticao/ContaLetra.java
+++ Java-repeticao/ContaLetra.java
@@ -3,17 +3,16 @@ class ContaLetra {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite uma palavra ou frase: ");
-        String texto = scanner.nextLine();
+        String texto=scanner.nextLine();
 
         int contador = 0;
 
-        for (int i = 0; i < texto.length(); i++) {
-            char c = texto.charAt(i);
+        for (int i=0; i<texto.length(); i++) {
+            char c=texto.charAt(i);
             if (Character.isLetter(c)) {
                 contador++;
             }
         }
-
         System.out.println("Quantidade de letras: " + contador);
         scanner.close();
     }
