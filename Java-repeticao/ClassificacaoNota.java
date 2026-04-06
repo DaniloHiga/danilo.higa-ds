cae47d3a3a154ea519822f6dd2967457927740b8
Danilo Higa <danikehiga@gmail.com>
2026-04-06 08:45:48 -0300

    exercicio 18

--- Java-repeticao/ClassificacaoNota.java
+++ Java-repeticao/ClassificacaoNota.java
@@ -3,13 +3,13 @@ class ClassificacaoNota {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite a nota (0 a 10): ");
-        double nota = scanner.nextDouble();
+        double nota=scanner.nextDouble();
 
-        if (nota >= 9) {
+        if (nota>=9) {
             System.out.println("MB");
-        } else if (nota >= 7) {
+        } else if (nota>=7) {
             System.out.println("B");
-        } else if (nota >= 5) {
+        } else if (nota>=5) {
             System.out.println("R");
         } else {
             System.out.println("I");
